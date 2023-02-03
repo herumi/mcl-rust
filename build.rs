cba4b4324f81a7f4bbb6280979cd140dc3bc5d76
MITSUNARI Shigeo <herumi@nifty.com>
2023-02-03 09:28:06 +0900

    use clang++ unless x86_64

--- build.rs
+++ build.rs
@@ -1,9 +1,18 @@
 use std::process::Command;
 
 fn main() {
-    let cmd = "mkdir -p build && cd build && cmake ../mcl -DMCL_STATIC_LIB=ON && make -j";
+    let opt = if cfg!(target_arch = "x86_64") {
+        ""
+    } else {
+        "-DCMAKE_CXX_COMPILER=clang++"
+    };
+
+    let cmd = format!(
+        "mkdir -p build && cd build && cmake ../mcl -DMCL_STATIC_LIB=ON {} && make -j",
+        opt
+    );
     let output = Command::new("sh")
-        .args(["-c", cmd])
+        .args(["-c", &cmd])
         .output()
         .expect(&"fail");
     if !output.status.success() {
