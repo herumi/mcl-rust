4b2878dbef8e9fb237f6891895e978afdafcb380
artiomtr <44021713+ArtiomTr@users.noreply.github.com>
2025-11-06 11:12:47 +0200

    Update build to use cmake crate

--- build.rs
+++ build.rs
@@ -1,25 +1,17 @@
-use std::env;
-use std::process::Command;
+use cmake::Config;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let top_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let opt = if cfg!(target_arch = "x86_64") {
-        ""
-    } else {
-        "-DCMAKE_CXX_COMPILER=clang++"
-    };
+    let mut config = Config::new("mcl");
 
-    let cmd = format!(
-        "cd {out} && cmake {top}/mcl -DMCL_STATIC_LIB=ON -DMCL_STANDALONE=ON {opt} && make -j",
-        out = out_dir,
-        top = top_dir,
-        opt = opt
-    );
-    Command::new("sh")
-        .args(["-c", &cmd])
-        .output()
-        .expect("fail");
-    let s = format!("cargo:rustc-link-search=native={}/lib", out_dir);
-    println!("{}", s);
+    config
+        .define("MCL_STATIC_LIB", "ON")
+        .define("MCL_STANDALONE", "ON");
+
+    if cfg!(target_arch = "x86_64") {
+        config.define("-DCMAKE_CXX_COMPILER", "clang++");
+    }
+
+    let dst = config.build();
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
 }
